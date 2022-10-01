80250e170075e81c1f0c22c70e8446d4cd24fc2b
leungchiying <leungchiyung1112@gmail.com>
2022-10-01 23:04:32 +0800

    kkk

---
+++ src/main/java/com/example/kkk/KkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kkk/control/ctl.java
@@ -0,0 +1,18 @@
+package com.example.kkk.control;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ni")
+
+public class ctl {
+    @RequestMapping("/hao")
+    public String nihao() {
+        return "nihao";
+    }
+}
+
+
+
+
